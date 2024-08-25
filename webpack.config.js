fc4be8e1658082c6d29451790bb17084c6691b19
Peng <PYWang0608@foxmail.com>
2024-08-25 03:55:32 +0200

    optimize

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
     output: {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, './docs'),
+        publicPath: '',
     },
     module: {
         rules: [
